9cefe1b09bbc683ba479b0e05290ab8183fe6194
Mert Dumenci <mert@dumenci.me>
2018-10-30 20:47:29 -0400

    Tidy up inversion

--- data/filters.py
+++ data/filters.py
@@ -1,6 +1,13 @@
+"""
+Instagram-like filters.
+
+Courtesy of Chunlok Lo.
+"""
+
 import numpy as np
 import skimage
-from skimage import io, filters
+import skimage.filters
+import skimage.color
 
 
 def channel_adjust(channel, values):
@@ -8,6 +15,7 @@ def channel_adjust(channel, values):
     flat_channel = channel.flatten()
     adjusted = np.interp(flat_channel, np.linspace(0, 1, len(values)), values)
     return adjusted.reshape(orig_size)
+
 
 def split_image_into_channels(image):
     """Look at each image separately"""
@@ -29,20 +37,6 @@ def sharpen(image, a, b):
     return sharper
 
 
-def channel_adjust(channel, values):
-    # preserve the original size, so we can reconstruct at the end
-    orig_size = channel.shape
-    # flatten the image into a single array
-    flat_channel = channel.flatten()
-
-    # this magical numpy function takes the values in flat_channel
-    # and maps it from its range in [0, 1] to its new squeezed and
-    # stretched range
-    adjusted = np.interp(flat_channel, np.linspace(0, 1, len(values)), values)
-
-    # put back into the original image shape
-    return adjusted.reshape(orig_size)
-
 def contrast_adjust(image, values):
     r, g, b = split_image_into_channels(image)
     r = channel_adjust(r, values)
@@ -51,11 +45,11 @@ def contrast_adjust(image, values):
     adjusted_im = merge_channels(r, g, b)
     return adjusted_im
 
-from skimage import color
+
 def change_saturation(image, offset):
-    hsv = color.rgb2hsv(image)
+    hsv = skimage.color.rgb2hsv(image)
     hsv[:, :, 1] = np.clip(hsv[:, :, 1] * offset, 0, 1)
-    return color.hsv2rgb(hsv)
+    return skimage.color.hsv2rgb(hsv)
 
 
 def gotham(image):
@@ -68,7 +62,7 @@ def gotham(image):
         0.95, 1.0])
     b_more = np.clip(b + 0.03, 0, 1.0)
     merged = np.stack([r_boost_lower, original_image[:, :, 1], b_more], axis=2)
-    blurred = filters.gaussian(merged, sigma=10, multichannel=True)
+    blurred = skimage.filters.gaussian(merged, sigma=10, multichannel=True)
     final = np.clip(merged * 1.3 - blurred * 0.3, 0, 1.0)
     b = final[:, :, 2]
     b_adjusted = channel_adjust(b, [
@@ -79,11 +73,10 @@ def gotham(image):
     final[:, :, 2] = b_adjusted
     return final
 
+
 def clarendon(image):
     original_image = skimage.img_as_float(image)
     im = original_image
-    #im = change_saturation(im, 1.5)
-    #im = contrast_adjust(im, [0, 0.1, 0.5, 0.7, 0.9, 1])
     r, g, b = split_image_into_channels(im)
     r = channel_adjust(r, [0, 0.20, 0.5, 0.9, 1])
     g = channel_adjust(g, [0, 0.25, 0.6, 0.8, 1])
@@ -91,11 +84,10 @@ def clarendon(image):
     im = merge_channels(r, g, b)
     return im
 
+
 def gingham(image):
     original_image = skimage.img_as_float(image)
     im = original_image
-    #im = change_saturation(im, 1.5)
-    #im = contrast_adjust(im, [0, 0.1, 0.5, 0.7, 0.9, 1])
     im = skimage.img_as_float(im)
     r, g, b = split_image_into_channels(im)
     r = channel_adjust(r, [0.2, 0.4, 0.7, 0.85, 1])
@@ -104,37 +96,33 @@ def gingham(image):
     im = merge_channels(r, g, b)
     return im
 
+
 def juno(image):
     #Juno filter
     original_image = skimage.img_as_float(image)
     im = original_image
-    #im = change_saturation(im, 1.5)
     im = contrast_adjust(im, [0, 0.15, 0.5, 0.85, 1])
     r, g, b = split_image_into_channels(im)
-    #r = channel_adjust(r, [0.2, 0.4, 0.7, 0.85, 1])
     g = channel_adjust(g, [0.1, 0.5, 1])
-    #b = channel_adjust(b, [0.2, 0.35, 0.6, 0.8, .9])
     im = merge_channels(r, g, b)
     return im
+
 
 def lark(image):
     #Lark filter
     original_image = skimage.img_as_float(image)
     im = original_image
-    #im = change_saturation(im, 1.5)
     im = contrast_adjust(im, [0, 0.2, 0.5, 0.85, 1])
     r, g, b = split_image_into_channels(im)
-    #r = channel_adjust(r, [0.2, 0.4, 0.7, 0.85, 1])
     g = channel_adjust(g, [0.1, 0.28, 0.55, 0.8, 1])
-    #b = channel_adjust(b, [0.2, 0.35, 0.6, 0.8, .9])
     im = merge_channels(r, g, b)
     return im
+
 
 def reyes(image):
     #Reyes filter
     original_image = skimage.img_as_float(image)
     im = original_image
-    #im = change_saturation(im, 1.5)
     im = contrast_adjust(im, [0.1, 0.5, 0.8, 0.9, 1])
     r, g, b = split_image_into_channels(im)
     r = channel_adjust(r, [0.0, 0.25, 0.55, 0.76, 1])
---
+++ utility.py
@@ -0,0 +1,11 @@
+""""
+Utility functions.
+
+Author: Mert Dumenci
+"""
+
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
